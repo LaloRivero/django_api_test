e1deeac62704d686a8deff08eec7841e9aa5bd48
Eduardo Rivero <lalo@lalorivero.com>
2021-12-22 16:14:25 -0700

    Companies model

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+''' Companies Models '''
 
-# Create your models here.
+# Natives
+import uuid
+
+# Django
+from django.db import models
+from django.db.models.fields import IntegerField
+
+
+class Company (models.Model):
+    ''' Company model that fit the application requirements  '''
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    ticker = models.CharField(max_length=10, blank=True, null=True)
+
+
