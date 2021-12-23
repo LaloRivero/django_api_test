50c0f82d9c333c7dc2a7b29882da6228edbb3391
Eduardo Rivero <lalo@lalorivero.com>
2021-12-23 11:37:57 -0700

    update function that handles PUT and PACTH behavior

--- companies/views.py
+++ companies/views.py
@@ -63,3 +63,20 @@ class CompanyViewSet(mixins.CreateModelMixin,
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.validated_data)
         return Response(serializer.validated_data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+    def update(self, request, *args, **kwargs):
+        ''' Handel update and partial update for a model instance '''
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        instance.values = self.convert_str_to_list(instance)
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
