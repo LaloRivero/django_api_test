d1e3b6bf538c98462f970bdf6f1041534a2e1d5d
Eduardo Rivero <lalo@lalorivero.com>
2021-12-23 11:04:15 -0700

    Str to list function converter

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+''' Company operations '''
 
-# Create your views here.
+# Django Rest Framework
+from rest_framework import status, mixins, viewsets
+from rest_framework.response import Response
+
+# Models
+from companies.models import Company
+
+# Serializer
+from companies.serializers import CompanySerializer
+
+
+class CompanyViewSet(mixins.CreateModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.DestroyModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+    def convert_str_to_list(self,data):
+        new_list = []
+        for num in data.values:
+            try:
+                new_list.append(int(num))
+            except:
+                pass
+
+        return new_list
